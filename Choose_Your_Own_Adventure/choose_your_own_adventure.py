a39077e076549e6dfd7b2d953c8ceebceb5bd438
Michael Caballero <mcaballero.dev@gmail.com>
2023-05-15 13:30:57 -0700

    add choose your own adventure project

---
+++ Choose_Your_Own_Adventure/choose_your_own_adventure.py
@@ -0,0 +1,16 @@
+class Character:
+    def __self__init(self):
+        self.name = name
+        self.age
+        self.health
+        self.status
+        self.inventory
+
+player = Character()
+
+introduction = "You awake in a dark cave. Your head hurts. Your feet feel tight. You can't see, you can only hear the dripping of water from the ceiling. You can barely remember your name.....What is your name?\n"
+
+print(introduction)
+player.name = input()
+
+print("Ah right.... Your name is", player.name + ", of course.")
